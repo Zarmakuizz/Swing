90d39921c5d504cfd71ce027fac2377b508d8eb9
MKRTCHIAN <mkrtchia@polytech.unice.fr>
2011-03-04 12:47:13 +0100

    scroll

--- src/vue/Accueil.java
+++ src/vue/Accueil.java
@@ -9,59 +9,75 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 /**
  * @author namor
  * @author Zarmakuizz
  */
 
-public class Accueil extends JFrame implements ActionListener{
+public class Accueil extends JFrame implements ActionListener {
     /** Boutons */
     private JButton add, mod, desc, supp;
     /** Le panel central */
-    private JPanel pCentral;
+    private JScrollPane pCentral;
     /** Le panel des boutons */
     private JPanel pBoutons;
-    
-    public Accueil(){
-        setSize(500, 400);
-        //Boutons
+    /** la liste des noms de groupe */
+    private JList lGroupe;
+    /** la liste des noms de personnes du groupe sélectionné */
+    private JList lPersonnes;
+
+    public Accueil() {
+        setSize(500, 200);
+        // Boutons
         add = new JButton("Ajouter");
         mod = new JButton("Modifier");
         supp = new JButton("Supprimer");
         desc = new JButton("Description");
         pBoutons = new JPanel();
         add(pBoutons, BorderLayout.NORTH);
-        pBoutons.add(add); pBoutons.add(mod); pBoutons.add(desc); pBoutons.add(supp);
-        //Listeners
+        pBoutons.add(add);
+        pBoutons.add(mod);
+        pBoutons.add(desc);
+        pBoutons.add(supp);
+        // Listeners
         add.addActionListener(this);
         mod.addActionListener(this);
         desc.addActionListener(this);
         supp.addActionListener(this);
+
         // Panel central
-        pCentral = new JPanel();
-        add(pCentral, BorderLayout.SOUTH);
+        String[] groupes = { "G1 blablabla", "G2blablabla", "G3",
+                "G4 blablabla", "G5", "G6", "G7", "G8", "G9 blablabla",
+                "G10blablabla", "G11", "G12 blablabla", "G13", "G14", "G15", "G16" }, personnes = {
+                "P1", "P2" };
+        lGroupe = new JList(groupes);
+        lPersonnes = new JList(personnes);
+        pCentral = new JScrollPane(lGroupe);
+        add(pCentral, BorderLayout.CENTER);
+        //pCentral.add(lGroupe); // pCentral.add(lPersonnes);
     }
 
     /**
      * Ecoute les évènements.
-     * @param e L'évènement qui survient
+     * 
+     * @param e
+     *            L'évènement qui survient
      */
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == add) {
             add.setText("lol");
-        }
-        else if (e.getSource() == mod) {
+        } else if (e.getSource() == mod) {
             mod.setText("lol");
-        }
-        else if (e.getSource() == desc) {
+        } else if (e.getSource() == desc) {
             desc.setText("lol");
-        }
-        else if (e.getSource() == supp) {
+        } else if (e.getSource() == supp) {
             supp.setText("lol");
         }
     }
-    
+
 }
