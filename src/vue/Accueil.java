fc7e9beedacbf1f788f46faf00bf6e026afb022e
MKRTCHIAN <mkrtchia@polytech.unice.fr>
2011-03-04 12:24:43 +0100

    du code

--- src/vue/Accueil.java
+++ src/vue/Accueil.java
@@ -3,6 +3,7 @@
  */
 package vue;
 
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -24,13 +25,43 @@ public class Accueil extends JFrame implements ActionListener{
     private JPanel pBoutons;
     
     public Accueil(){
-        
+        setSize(500, 400);
+        //Boutons
+        add = new JButton("Ajouter");
+        mod = new JButton("Modifier");
+        supp = new JButton("Supprimer");
+        desc = new JButton("Description");
+        pBoutons = new JPanel();
+        add(pBoutons, BorderLayout.NORTH);
+        pBoutons.add(add); pBoutons.add(mod); pBoutons.add(desc); pBoutons.add(supp);
+        //Listeners
+        add.addActionListener(this);
+        mod.addActionListener(this);
+        desc.addActionListener(this);
+        supp.addActionListener(this);
+        // Panel central
+        pCentral = new JPanel();
+        add(pCentral, BorderLayout.SOUTH);
     }
 
+    /**
+     * Ecoute les évènements.
+     * @param e L'évènement qui survient
+     */
     @Override
-    public void actionPerformed(ActionEvent arg0) {
-        // TODO Auto-generated method stub
-        
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == add) {
+            add.setText("lol");
+        }
+        else if (e.getSource() == mod) {
+            mod.setText("lol");
+        }
+        else if (e.getSource() == desc) {
+            desc.setText("lol");
+        }
+        else if (e.getSource() == supp) {
+            supp.setText("lol");
+        }
     }
     
 }
